d82d08e8f8d817e044609d6c3e3fe4e6b716baea
81135998191 <81135998191@dti-desen15>
2019-05-07 17:20:55 -0300

    Finalizando cards do carrinho de compras

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -10,7 +10,15 @@ class CartModel extends Model {
 
   List<CartProduct> products = [];
 
-  CartModel(this.user);
+  String codigoCupom;
+  int porcentagemDesconto = 0;
+
+  bool estaCarregando = false;
+
+  CartModel(this.user) {
+    if (user.isLoggedIn())
+      _loadCartItems();
+  }
 
   static CartModel of(BuildContext context) =>
       ScopedModel.of<CartModel>(context);
@@ -33,4 +41,55 @@ class CartModel extends Model {
     notifyListeners();
   }
 
+  void decProduct(CartProduct cartProduct) {
+    cartProduct.quantity --;
+
+    Firestore.instance.collection("users").document(user.firebaseUser.uid).collection("cart")
+    .document(cartProduct.cid).updateData(cartProduct.toMap());
+    notifyListeners();
+  }
+
+  void incProduct(CartProduct cartProduct) {
+    cartProduct.quantity ++;
+
+    Firestore.instance.collection("users").document(user.firebaseUser.uid).collection("cart")
+        .document(cartProduct.cid).updateData(cartProduct.toMap());
+    notifyListeners();
+  }
+
+  void setCoupon(String couponCode, int discountPercentage) {
+    this.codigoCupom = couponCode;
+    this.porcentagemDesconto = discountPercentage;
+  }
+
+  void updatePrices() {
+    notifyListeners();
+  }
+
+  double getProductsPrice() {
+    double price = 0.0;
+
+    for (CartProduct card in products) {
+      if (card.productData != null)
+        price += card.quantity * card.productData.price;
+    }
+    return price;
+  }
+
+  double getDiscount() {
+    return getProductsPrice() * (porcentagemDesconto / 100);
+  }
+
+  double getShipPrice() {
+    return 9.99;
+  }
+
+  void _loadCartItems() async{
+    QuerySnapshot query = await Firestore.instance.collection("users").document(user.firebaseUser.uid).collection("cart")
+        .getDocuments();
+
+    products = query.documents.map((doc) => CartProduct.fromDocument(doc)).toList();
+
+    notifyListeners();
+  }
 }
